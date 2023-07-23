e5ec37eb84f17ea2fdf23684c05697a5fc666f60
lyang <altair0319@gmail.com>
2023-07-22 18:37:21 -0700

    LY - using oculus integration in Metaframe

--- Assets/0B_Prototype/Jason/Scripts/Data/SkillInfoListSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SkillListSO", menuName = "ScriptableObjects/SkillSO")]
-public class SkillInfoListSO : ScriptableObject
-{
-    public List<SkillInfo> SkillList = new List<SkillInfo>();
-}
